29340dabfeeac9e23904cf30892e18a40f48d9f8
Rita Vasques <103426219+TheDeveloperQueen@users.noreply.github.com>
2024-10-09 12:53:55 +0200

    Add files via upload

--- move.c
+++ move.c
@@ -6,47 +6,11 @@
 /*   By: ritavasques <ritavasques@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/02 16:30:38 by ritavasques       #+#    #+#             */
-/*   Updated: 2024/10/09 12:34:46 by ritavasques      ###   ########.fr       */
+/*   Updated: 2024/10/09 12:53:32 by ritavasques      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3D.h"
-
-int key_press(int keycode, t_data *data)
-{
-    if (keycode == ESC)
-        close_window(data);
-    if (keycode == W)
-        data->player.move_y = 1;
-    if (keycode == A)
-        data->player.move_x = -1;
-    if (keycode == D)
-        data->player.move_x = 1;
-    if (keycode == S)
-        data->player.move_y = -1;
-    if (keycode == RIGHT)
-        data->player.rotate += 1;
-    if (keycode == LEFT)
-        data->player.rotate -= 1;
-    return (0);
-}
-
-int key_release(int keycode, t_data *data)
-{
-    if (keycode == W && data->player.move_y == 1)
-        data->player.move_y = 0;
-    if (keycode == A && data->player.move_x == -1)
-        data->player.move_x == 0;
-    if (keycode == D && data->player.move_x == 1)
-        data->player.move_x = 0;
-    if (keycode == S && data->player.move_y == -1)
-        data->player.move_y = 0;
-    if (keycode == RIGHT && data->player.rotate <= 1)
-        data->player.rotate = 0;
-    if (keycode == LEFT && data->player.rotate >= 1)
-        data->player.rotate = 0;      
-    return (0);
-}
 
 int move_forward(t_data *data)
 {
